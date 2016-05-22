031dbc21bf055feaccf54e1778a53517b6e30127
Rishabh Jain <mailrishabh1403@gmail.com>
2016-05-22 14:24:16 +0530

    first js

---
+++ draw.js
@@ -0,0 +1,29 @@
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var radius = 10;
+var drag = false;
+context.lineWidth = radius*2;
+var putPoint = function (e) {
+  if(drag){
+    context.lineTo(e.clientX,e.clientY)
+    context.stroke();
+    context.beginPath();
+    context.arc(e.clientX,e.clientY,radius,0,Math.PI*2);
+    context.fill();
+    context.beginPath();
+    context.moveTo(e.clientX,e.clientY);
+  }
+}
+var engage = function(e){
+  drag = true;
+  putPoint(e);
+}
+var disengage = function(){
+  drag = false;
+  context.beginPath();
+}
+canvas.addEventListener('mouseup',disengage);
+canvas.addEventListener('mousedown',engage);
+canvas.addEventListener('mousemove',putPoint);
