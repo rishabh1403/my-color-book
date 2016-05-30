7be30435140d60373c2e9c7e2de6ead132c10b04
Rishabh Jain <mailrishabh1403@gmail.com>
2016-05-30 22:02:49 +0530

    draw logic pushed to ctrl

--- main.js
+++ main.js
@@ -2,7 +2,35 @@
   var app = angular.module("myColorBook",[]);
   app.controller('mainCtrl',['$scope',function($scope){
 
-$scope.show = "hello";
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    var radius = 10;
+    var drag = false;
+    context.lineWidth = radius*2;
+    var putPoint = function (e) {
+      if(drag){
+        context.lineTo(e.clientX,e.clientY)
+        context.stroke();
+        context.beginPath();
+        context.arc(e.clientX,e.clientY,radius,0,Math.PI*2);
+        context.fill();
+        context.beginPath();
+        context.moveTo(e.clientX,e.clientY);
+      }
+    }
+    var engage = function(e){
+      drag = true;
+      putPoint(e);
+    }
+    var disengage = function(){
+      drag = false;
+      context.beginPath();
+    }
+    canvas.addEventListener('mouseup',disengage);
+    canvas.addEventListener('mousedown',engage);
+    canvas.addEventListener('mousemove',putPoint);
 
   }]);
 
