0c0a040bc35d26d95b02fbb9d7be421a41370df2
Rishabh Jain <mailrishabh1403@gmail.com>
2016-05-31 01:19:17 +0530

    almost complete

--- main.js
+++ main.js
@@ -12,14 +12,22 @@
     var radius = 10;
     var drag = false;
     var imageObj = new Image();
-context.globalCompositeOperation = "multiply";
+    //context.globalCompositeOperation='source-ove';
+    //context.globalCompositeOperation = "multiply";
       imageObj.onload = function() {
         context.drawImage(imageObj, 69, 50);
-      };
+     };
       imageObj.src = 'download.png';
+      $scope.eraseParts = function(){
+        context.strokeStyle = 'white';
+        context.fillStyle = 'white';
+        //context.globalCompositeOperation='source-over';
+      }
     $scope.colorChange = function(color){
       context.strokeStyle = color;
       context.fillStyle = color;
+    //  context.globalCompositeOperation='multiply';
+
     };
     $scope.radiusChange = function(size) {
       radius = size;
@@ -29,17 +37,27 @@ context.globalCompositeOperation = "multiply";
     context.lineWidth = radius*2;
     var putPoint = function (e) {
       if(drag){
+        //context.globalCompositeOperation='multiply';
         context.lineTo(e.offsetX,e.offsetY)
         context.stroke();
         context.beginPath();
+        //context.globalCompositeOperation='source-over';
         context.arc(e.offsetX,e.offsetY,radius,0,Math.PI*2);
+
         context.fill();
+        //context.globalCompositeOperation='multiply';
         context.beginPath();
         context.moveTo(e.offsetX,e.offsetY);
+        context.globalCompositeOperation='source-atop';
+
+        context.drawImage(imageObj, 69, 50);
+        context.globalCompositeOperation='source-over';
+        //context.drawImage(imageObj, 69, 50);
+
       }
     }
-    var imageData = context.getImageData(0,0,canvas.width,canvas.height);
-    console.log(imageData);
+    //var imageData = context.getImageData(0,0,canvas.width,canvas.height);
+    //console.log(imageData);
     var engage = function(e){
       drag = true;
       putPoint(e);
